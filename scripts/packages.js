3b09c4691a46bc2a022ad6d6a700a452724f6c91
wifislax678 <109185236+wifislax678@users.noreply.github.com>
2022-08-06 21:46:19 +0800

    Update packages.js

--- scripts/packages.js
+++ scripts/packages.js
@@ -5,14 +5,14 @@
  * @desc app 描述
  */
 module.exports = [
-  {
-    name: 'luci-theme-argon',
-    desc: 'Argon主题',
-  },
-  {
-    name: 'luci-app-argon-config',
-    desc: 'Argon主题设置',
-  },
+//  {
+ //   name: 'luci-theme-argon',
+ //   desc: 'Argon主题',
+ // },
+//  {
+ //   name: 'luci-app-argon-config',
+ //   desc: 'Argon主题设置',
+//  },
   {
     name: 'luci-app-arpbind',
     desc: 'IP/MAC绑定',
@@ -25,30 +25,30 @@ module.exports = [
     name: 'luci-app-ddns',
     desc: '动态域名 DDNS',
   },
-  {
-    name: 'luci-app-ddnsto',
-    desc: 'DDNSTO 内网穿透',
-  },
+ // {
+  //  name: 'luci-app-ddnsto',
+  //  desc: 'DDNSTO 内网穿透',
+ // },
   {
     name: 'luci-app-diskman',
     desc: '磁盘管理工具',
   },
-  {
-    name: 'luci-app-nlbwmon',
-    desc: '网络带宽监视器',
-  }, 
-  // {
-  //   name: 'luci-app-passwall',
-  //   desc: 'PassWall 代理',
-  // },
+ // {
+ //   name: 'luci-app-nlbwmon',
+ //   desc: '网络带宽监视器',
+ // }, 
+   {
+     name: 'luci-app-passwall',
+     desc: 'PassWall 代理',
+   },
   {
     name: 'luci-app-ramfree',
     desc: '释放内存',
   },
-  {
-    name: 'luci-app-serverchan',
-    desc: '微信/Telegram推送',
-  },
+  // {
+  //  name: 'luci-app-serverchan',
+  //  desc: '微信/Telegram推送',
+ // },
   {
     name: 'luci-app-ssr-plus',
     desc: 'SSR-Plus 代理',
@@ -57,34 +57,34 @@ module.exports = [
     name: 'luci-app-transmission',
     desc: 'Transmission下载器',
   },
-  {
-    name: 'luci-app-unblockmusic',
-    desc: '解网易云歌曲3合1',
-  },
-  {
-    name: 'luci-app-unblockneteasemusic-go',
-    desc: '解网易云歌曲-GO',
-  },
-  {
-    name: 'luci-app-unblockneteasemusic',
-    desc: '解网易云歌曲-MINI',
-  },
+ // {
+  //  name: 'luci-app-unblockmusic',
+  //  desc: '解网易云歌曲3合1',
+ // },
+ // {
+ //   name: 'luci-app-unblockneteasemusic-go',
+ //   desc: '解网易云歌曲-GO',
+ // },
+ // {
+  //  name: 'luci-app-unblockneteasemusic',
+  //  desc: '解网易云歌曲-MINI',
+  // },
   {
     name: 'luci-app-upnp',
     desc: '通用即插即用UPnP（端口自动转发）',
   },
-  {
-    name: 'luci-app-vlmcsd',
-    desc: 'KMS服务器',
-  },
-  {
-    name: 'luci-app-wol',
-    desc: 'Wol 网络唤醒',
-  },
-  {
-    name: 'luci-app-wrtbwmon',
-    desc: '实时流量监测',
-  },
+  // {
+   // name: 'luci-app-vlmcsd',
+  //  desc: 'KMS服务器',
+ // },
+  // {
+  //  name: 'luci-app-wol',
+   // desc: 'Wol 网络唤醒',
+  // },
+  // {
+   // name: 'luci-app-wrtbwmon',
+  //  desc: '实时流量监测',
+ // },
   {
     name: 'luci-compat',
     desc: 'Compat依赖',
